4b472370e176bbe449f1c88df8056f5fef015e5e
vikramsingh863 <vikramsinghrathore863@gmail.com>
2024-01-25 18:03:11 +0530

    completed 25-01-2024

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,91 @@ import { useState, useEffect } from 'react'
 import useCurrencyInfo from './hooks/useCurrencyInfo'
 import InputBox from './components/InputBox'
 // import './App.css'
-// const currencyInfo= useCurrencyInfo("inr")
+
 function App() {
-  const [count, setCount] = useState(0)
-  
+  const [amount, setamount] = useState(1)
+  const [from, setFrom] = useState("usd")
+  const [to, setTo] = useState("inr")
+  const [ConvertedAmount, setConvertedAmount] = useState(0)
+
+  const currencyInfo = useCurrencyInfo(from)
+  let Options = Object.keys(currencyInfo);
+  console.log(Options)
+
+  const swap = () => {
+    setFrom(to)
+    setTo(from)
+    setConvertedAmount(setamount)
+    setamount(setConvertedAmount)
+  }
+
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo[to])
+  }
   return (
     <>
-    <div className='main'>
-    <InputBox
-    
-    />
-    <div className='bg-orange-500'> </div>
-    </div>
+
+      <div className='main'>
+        <div className='box'>
+          <div className='contain'>
+          <form onSubmit={(e) => {
+            e.preventDefault();
+            convert()
+          }}>
+
+            <InputBox
+              label={"from"}
+              amount={amount}
+              currencyOption={Options}
+              onCurrencyChange={(currency) => setamount(amount)}
+              selectCurrency={from}
+
+            />
+            <div className='buttons'>
+              <button className='buttons-style'
+                type="button"
+
+                onClick={swap}
+              > swap
+              </button>
+
+              <button type='button'className='buttons-style' onClick={() => {
+                if (amount > 1) {
+                  setamount(amount - 1);
+                }
+              }}>-</button>
+
+              <button type='button' className='buttons-style' onClick={() => {
+                setamount(1)
+                setConvertedAmount(0);
+              }}>0</button>
+
+              <button type='button' className='buttons-style' onClick={() => {
+                setamount(amount + 1);
+              }}>+</button>
+
+            </div>
+
+
+            <InputBox
+              label={"to"}
+              amount={ConvertedAmount}
+              currencyOption={Options}
+              onCurrencyChange={(currency) => setTo(currency)}
+              selectCurrency={to}
+              amountDisable
+
+            />
+
+            <div className="btn">
+              <button>Convert to </button>
+            </div>
+
+
+          </form>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
